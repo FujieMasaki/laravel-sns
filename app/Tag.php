0c2151ec89e5fd63d73c88031cb71cb18632acc8
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-09-04 17:48:47 +0900

    タグ機能追加

--- app/Tag.php
+++ app/Tag.php
@@ -3,10 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     protected $fillable = [
         'name',
     ];
+
+    public function getHashtagAttribute(): string
+    {
+        return '#' . $this->name;
+        // タグ名の先頭に、'#'を付けて返すことになる
+    }
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Article')->withTimestamps();
+    }
 }
