fb63dbb3ad51600a2fc52fa70bdd995e6dc4dc9f
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-09-07 22:15:25 +0900

    フォロー中・フォロワーの数表示

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Foundation\Console\Presets\React;
 
 class UserController extends Controller
 {
@@ -14,5 +15,32 @@ class UserController extends Controller
         return view('users.show', [
             'user' => $user,
         ]);
+    }
+
+    public function follow(Request $request, string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        if ($user->id === $request->user()->id) {
+            return abort('404', 'Cannot follow yourself.');
+        }
+
+        $request->user()->followings()->detach($user);
+        $request->user()->followings()->attach($user);
+
+        return ['name' => $name];
+    }
+
+    public function unfollow(Request $request, string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        if ($user->id === $request->user()->id) {
+            return abort('404', 'Cannot follow yourself.');
+        }
+
+        $request->user()->followings()->detach($user);
+
+        return ['name' => $name];
     }
 }
